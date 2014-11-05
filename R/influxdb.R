f84f5060bbfefcf312f8d922ecfb7e0ca0836856
svernede <svernede@baselinechina.com>
2014-11-05 11:32:16 +0800

    Added an influxdb_write function. The input data is a named list of data frames where the names are the series names, and the data frames contain the points. influxdb_write is therefore quite symmetric to the  influxdb_query function.

--- R/influxdb.R
+++ R/influxdb.R
@@ -31,13 +31,8 @@ influxdb_query <- function(host, port, username, password, database, query,
     )
   )
   
-  # Check for error. Not familiar enough with httr, there may be other ways it
-  # communicates failure.
-  if (response$status_code < 200 || response$status_code >= 300) {
-    if (length(response$content) > 0)
-      warning(rawToChar(response$content))
-    stop("Influx query failed with HTTP status code ", response$status_code)
-  }
+  ## original code moved to a function to share with influxdb_write 
+  check_reponse_status(response)
   
   response_data <- fromJSON(rawToChar(response$content))
   
@@ -54,4 +49,73 @@ influxdb_query <- function(host, port, username, password, database, query,
     structure(list(df), names=seriesObj$name)
   })
   return(responseObjects)
+}
+
+
+#' Write data to an InfluxDB database
+#' 
+#' @param host Character vector with IP address or hostname
+#' @param port Port number
+#' @param username InfluxDB user
+#' @param password InfluxDB password (will be passed in plain text)
+#' @param database The name of the database
+#' @param series A named list of data frames, where the names are the series names, and the data frames contain the points.
+#' @param time_precision Specifies whether the time should be returned in 
+#'   seconds (\code{s}), milliseconds (\code{m}), or microseconds (\code{u}) 
+#'   from epoch (January 1, 1970, 00:00:00 UTC).
+#' @return The httr response object (invisibly) 
+#'
+#' @export
+influxdb_write <- function(host, port, username, password, database, series , time_precision=c("s", "m", "u")) {
+  
+  data_formated <- mapply(format_data,names(series),series,USE.NAMES = FALSE,SIMPLIFY = FALSE)
+  
+  data_json <-  toJSON(data_formated) 
+  
+  response <- POST(
+    "", scheme = "http", hostname = host, port = port,
+    path = sprintf("db/%s/series", URLencode(database)),
+    query = list(
+      u = username,
+      p = password,
+      time_precision = match.arg(time_precision)
+    ),
+    body=data_json
+  )
+  
+  check_reponse_status(response)
+  
+  invisible(response)
+}
+
+check_reponse_status<-function(response){
+  # Check for error. Not familiar enough with httr, there may be other ways it
+  # communicates failure.
+  if (response$status_code < 200 || response$status_code >= 300) {
+    if (length(response$content) > 0)
+      warning(rawToChar(response$content))
+    stop("Influx query failed with HTTP status code ", response$status_code)
+  }
+  
+}
+
+format_data <- function(name,df){
+  columns <- as.list(colnames(df))
+  split_list <- split(df, 1:nrow(df)) 
+  
+  # We need to drop all names from the points structure 
+  # for rjson to produce the expected output
+  # There is probably a much more optimized way to do so
+  points<-mapply(drop_names,split_list,USE.NAMES = FALSE,SIMPLIFY = FALSE)  
+  list(
+    name = name,
+    columns = columns,
+    points =  points
+  )
+}
+
+drop_names <- function(x){
+  x_list <- as.list(x)
+  names(x_list) <-NULL
+  x_list
 }
